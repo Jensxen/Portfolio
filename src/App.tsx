669095e04eb31b90a43f259c10a635ec5deee262
Jensxen <marcusjensen.privat@gmail.com>
2025-11-03 18:21:19 +0100

    cursor fix around logo

--- src/App.tsx
+++ src/App.tsx
@@ -17,32 +17,42 @@ export default function App() {
     >
       <TargetCursor
         targetSelector=".cursor-target"
-        spinDuration={2}
+        spinDuration={3}
         hideDefaultCursor={true}
       />
 
-      {/* Logo in top-left */}
       <a
         href="/"
-        className="fixed top-4 left-4 z-20 w-[3.45rem] h-[3.45rem] cursor-target"
+        className="fixed top-4 left-4 z-20 w-19 h-15 cursor-target flex items-center justify-center"
         aria-label="Home logo"
       >
         <svg
           viewBox="0 0 964.25709 743.22518"
           xmlns="http://www.w3.org/2000/svg"
-          width="100%"
-          height="100%"
           role="img"
           aria-label="Logo"
-          className="w-full h-full object-contain"
-          style={{ filter: 'brightness(1)' }}
+          style={{ 
+            filter: "brightness(1)",
+            width: "60px",
+            height: "60px"
+          }}
         >
-          <polygon fill="#fff" points="964.25709 0 964.25709 180.7845 824.03804 180.7845 613.12137 391.69976 783.86066 562.44068 964.25709 562.44068 964.25709 743.22518 703.5059 743.22518 141.0226 180.7845 0 180.7845 0 0 221.37736 0 482.53032 261.11861 743.68328 0 964.25709 0"/>
-          <polygon fill="#fff" points="642.83806 743.22518 382.08687 743.22518 0 361.16726 0 220.95884 120.93391 220.95884 642.83806 743.22518"/>
-          <polygon fill="#fff" points="321.41903 743.22518 60.66784 743.22518 0 682.56194 0 542.35351 120.93391 542.35351 321.41903 743.22518"/>
+          <polygon
+            fill="#fff"
+            points="964.25709 0 964.25709 180.7845 824.03804 180.7845 613.12137 391.69976 783.86066 562.44068 964.25709 562.44068 964.25709 743.22518 703.5059 743.22518 141.0226 180.7845 0 180.7845 0 0 221.37736 0 482.53032 261.11861 743.68328 0 964.25709 0"
+          />
+          <polygon
+            fill="#fff"
+            points="642.83806 743.22518 382.08687 743.22518 0 361.16726 0 220.95884 120.93391 220.95884 642.83806 743.22518"
+          />
+          <polygon
+            fill="#fff"
+            points="321.41903 743.22518 60.66784 743.22518 0 682.56194 0 542.35351 120.93391 542.35351 321.41903 743.22518"
+          />
         </svg>
       </a>
 
+      {/* Dark Veil Background */}
       <div className="fixed inset-0 z-0">
         <DarkVeil
           hueShift={0}
@@ -54,10 +64,15 @@ export default function App() {
           resolutionScale={isMobile ? 0.6 : 1}
         />
       </div>
-
       <main className="relative z-10 p-8">
         {/* Social Icons */}
-        <div className={`fixed z-10 ${isMobile ? 'left-1/2 bottom-8 -translate-x-1/2' : 'left-1/2 top-1/2 -translate-x-1/2 translate-y-1/2'}`}>
+        <div
+          className={`fixed z-10 ${
+            isMobile
+              ? "left-1/2 bottom-8 -translate-x-1/2"
+              : "left-1/2 top-1/2 -translate-x-1/2 translate-y-1/2"
+          }`}
+        >
           <FadingIconsGroup
             icons={[
               {
